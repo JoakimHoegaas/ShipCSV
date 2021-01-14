c2e46b150151b15bf5a1bafeb6ee34289d36f01b
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-01-14 11:29:41 +0100

    updeeeit

--- ShipCSV/Program.cs
+++ ShipCSV/Program.cs
@@ -22,8 +22,8 @@ namespace ShipCSV
                     var ship = shipYard.CreateShip();
                     var owner = shipYard.CreateOwner(ship);
                     var countedOwner = owner.CountShipsPerOwner();
-                    ship.Owner = countedOwner;
-                    if (countedOwner.ShipCount == 2) // when there're two instances in Owners..
+                    ship.Owner = countedOwner; //two-way object reference set for print/WriteLine
+                    if (countedOwner.ShipCount == 2) // when there're two instances in Owners:
                     {
                         ships.Add(countedOwner.Ship); //add current/first ship instance
                         countedOwner.RemoveOwner(); //remove to isolate remaining instance
@@ -32,12 +32,13 @@ namespace ShipCSV
                         countedOwner.AddOwner(); //re-adds owner to maintain count/algorithm
                     }
                     if (countedOwner.ShipCount > 2) ships.Add(countedOwner.Ship);
-                    // more than two instances means the owner has been treated, and can be added directly
+                    // more than two instances means the owner has been treated, and can be added directly,
+                    // bypassing the steps above which are designed for handling the first occurance of serial-ownership 
                 }
             }
             foreach (var ship in ships)
             {
-                Console.WriteLine($"{ship.Id} {ship.Name} {ship.Owner.Name}");
+                Console.WriteLine($"{ship.Id} {ship.Name} {ship.Owner.Name} {ship.Owner.ShipCount}");
             }
         }
     }
